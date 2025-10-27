f276c7b6aa5c4a779fff2cb2971a792c936dd2d3
Mario García Luque <garcialuquemario7@gmail.com>
2025-10-27 14:39:24 +0100

    Reloj

---
+++ C#/Ej_Anonimas/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding.Exercise
+{
+    /*
+     * Para el delegado ValidarEdades, se pide declarar un Método Anónimo que lo
+     * implemente. Dicho método debe validar que la edad recibida por parámetro
+     * corresponda a alguien mayor de edad. Es decir que ese número debe ser mayor a 18, en
+     * cuyo caso el método deberá retornar el valor booleando True. Caso contrario retornara
+     * un valor False.
+     */
+    public delegate bool ValidarEdades(int x);
+
+    /*
+     * Dada la solución del ejercicio anterior, se pide declarar un método con el
+     * nombre ImprimirConsola. Este método debe imprimir en consola el resultado de
+     * ejecutar la implementación validate del delegado. Es decir, que se debe "mudar" la linea
+     * con la impresión en consola del Main al método ImprimirConsola, que además va a
+     * recibir 2 parámetros. Uno de ellos será el método del delegado mediante el
+     * objeto validate y el otro va a ser un int que contiene el valor de la edad que se debe
+     * validar. De más esta decir, que este método se debe invocar desde el Main pasandole
+     * los parámetros que correspondan al delegado y a la edad, cuyo valor debe ser 19.
+     */
+    public class Exercise
+    {
+        public delegate bool ValidarEdades(int x);
+
+        public static void Main()
+        {
+            ValidarEdades validate = (x) => x >= 18;
+            ImprimirConsola(validate, 19);
+        }
+
+        public static void ImprimirConsola(ValidarEdades validar, int edad)
+        {
+            Console.WriteLine(validar(edad));
+        }
+    }
+}
---
+++ C#/Reloj/Reloj/Reloj/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+class Reloj
+{
+    public delegate void SegundoCambiadoHandler(object sender, EventArgs e);
+    public event SegundoCambiadoHandler SegundoCambiado;
+
+    public void Iniciar()
+    {
+        while (true)
+        {
+            Thread.Sleep(1000);
+            OnSegundoCambiado();
+        }
+    }
+
+    protected virtual void OnSegundoCambiado()
+    {
+        if (SegundoCambiado != null)
+        {
+            SegundoCambiado(this, EventArgs.Empty);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Reloj reloj = new Reloj();
+        reloj.SegundoCambiado += Reloj_SegundoCambiado;
+
+        reloj.Iniciar();
+    }
+
+    static void Reloj_SegundoCambiado(object sender, EventArgs e)
+    {
+        Console.WriteLine("¡Ha pasado un segundo!");
+    }
+}
